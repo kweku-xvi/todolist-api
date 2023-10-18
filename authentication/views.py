1c150c35d6048712e3a9bd26da8acc6e32680ba6
kweku-xvi <nksarps@gmail.com>
2023-10-18 22:05:59 +0000

    authentication app views

--- authentication/views.py
+++ authentication/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from rest_framework.generics import GenericAPIView
-from .serializers import RegisterSerializer
+from .serializers import RegisterSerializer, LoginSerializer
 from rest_framework import response, status
+from django.contrib.auth import authenticate
 
 
 class RegisterAPIView(GenericAPIView):
@@ -14,3 +15,19 @@ class RegisterAPIView(GenericAPIView):
             serializer.save()
             return response.Response(serializer.data, status=status.HTTP_201_CREATED)
         return response.Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    
+class LoginAPIView(GenericAPIView):
+    serializer_class = LoginSerializer
+
+    def post(self, request):
+        email = request.data.get('email', None)
+        password = request.data.get('password', None)
+
+        user = authenticate(username=email,password=password)
+
+        if user:
+            serializer = self.serializer_class(user)
+
+            return response.Response(serializer.data, status=status.HTTP_200_OK)
+        return response.Response({'message':'Invalid credentials. Try again.'}, status=status.HTTP_401_UNAUTHORIZED)
