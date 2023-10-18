0abfa64f0bceb1ab9480801b63e46e4b482796b4
kweku-xvi <nksarps@gmail.com>
2023-10-18 20:22:02 +0000

    tests for custom user model

--- authentication/tests/test_models.py
+++ authentication/tests/test_models.py
@@ -34,10 +34,10 @@ class TestModel(APITestCase):
         self.assertEqual(user.email, 'cryce@truly.com')
 
 
-    def test_created_super_user_with_staff_status(self):
+    def test_creates_super_user_with_staff_status(self):
         with self.assertRaisesMessage(ValueError, "Superuser must have is_staff=True."):
             User.objects.create_superuser(username='cryce', email='', password='testing321', is_staff=False)
 
-    def test_created_super_user_with_super_user_status(self):
+    def test_creates_super_user_with_super_user_status(self):
         with self.assertRaisesMessage(ValueError, "Superuser must have is_superuser=True."):
             User.objects.create_superuser(username='cryce', email='', password='testing321', is_superuser=False)
