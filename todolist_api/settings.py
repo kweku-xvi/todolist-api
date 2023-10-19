dec72ed924e983cfb8df8b60ef6b0f00f3314e5a
kweku-xvi <nksarps@gmail.com>
2023-10-19 21:23:48 +0000

    main app settings

--- todolist_api/settings.py
+++ todolist_api/settings.py
@@ -120,7 +120,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-AUTH_USER_MODEL = 'authentication.User'
+AUTH_USER_MODEL = 'authentication.User' # setting the auth user model
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
