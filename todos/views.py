24f7a18cef888b6db75e3e32569dbb8fe2f921a1
kweku-xvi <nksarps@gmail.com>
2023-10-29 21:41:12 +0000

    added views to create, list, update, retrieve & delete todos

--- todos/views.py
+++ todos/views.py
@@ -1,3 +1,40 @@
 from django.shortcuts import render
+from rest_framework.generics import CreateAPIView, ListAPIView, ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from .serializers import TodoSerializer
+from rest_framework import permissions
+from .models import Todo
 
-# Create your views here.
+# class CreateTodoAPIView(CreateAPIView):
+#     serializer_class = TodoSerializer
+#     permission_classes = (permissions.IsAuthenticated,)
+
+#     def perform_create(self, serializer):
+#         return serializer.save(owner=self.request.user)
+
+
+# class TodoListAPIView(ListAPIView):
+#     serializer_class = TodoSerializer
+#     permission_classes = (permissions.IsAuthenticated,)
+
+#     def get_queryset(self):
+#         return Todo.objects.filter(owner=self.request.user)
+
+
+class TodosAPIView(ListCreateAPIView):
+    serializer_class = TodoSerializer
+    permission_classes = (permissions.IsAuthenticated,)
+
+    def perform_create(self, serializer):
+        return serializer.save(owner=self.request.user)
+
+    def get_queryset(self):
+        return Todo.objects.filter(owner=self.request.user)
+
+
+class TodoDetailAPIView(RetrieveUpdateDestroyAPIView):
+    serializer_class = TodoSerializer
+    permission_classes = (permissions.IsAuthenticated,)
+    lookup_field = 'id'
+
+    def get_queryset(self):
+        return Todo.objects.filter(owner=self.request.user)
