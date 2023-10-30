28a273bcb32d34d8a62882a637e8c6775057a747
kweku-xvi <nksarps@gmail.com>
2023-10-30 21:35:37 +0000

    adding pagination, filtering, search & ordering

--- todos/views.py
+++ todos/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from rest_framework.generics import CreateAPIView, ListAPIView, ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from .serializers import TodoSerializer
-from rest_framework import permissions
+from rest_framework import permissions, filters
 from .models import Todo
+from django_filters.rest_framework import DjangoFilterBackend
+from .pagination import CustomPageNumberPagination
 
 # class CreateTodoAPIView(CreateAPIView):
 #     serializer_class = TodoSerializer
@@ -22,7 +24,13 @@ from .models import Todo
 
 class TodosAPIView(ListCreateAPIView):
     serializer_class = TodoSerializer
+    pagination_class = CustomPageNumberPagination
     permission_classes = (permissions.IsAuthenticated,)
+
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
+    filterset_fields = ['id', 'title', 'is_complete']
+    search_fields = ['id', 'title', 'description', 'is_complete']
+    ordering_fields = ['id', 'title', 'description', 'is_complete']
 
     def perform_create(self, serializer):
         return serializer.save(owner=self.request.user)
