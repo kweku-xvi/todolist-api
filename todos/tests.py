9b88cb97a640e58f8f75fe37dd33b6455eab122a
kweku-xvi <nksarps@gmail.com>
2023-10-31 19:10:58 +0000

    adding tests for "tododetailapiview" view

--- todos/tests.py
+++ todos/tests.py
@@ -4,29 +4,36 @@ from rest_framework import status
 from .models import Todo
 
 
-class TestListCreateTodos(APITestCase):
+class TodosAPITestCase(APITestCase):
+    def create_todo(self):
+        sample_todos = {
+            'title':'test',
+            'description': 'hello world'
+        }
+        response = self.client.post(reverse('todos'), sample_todos)
+
+        return response
+
     def authenticate(self):
         self.client.post(reverse('register'), {'username':'username', 'email':'email@gmail.com', 'password':'password'})
         response = self.client.post(reverse('login'), {"email":"email@gmail.com", "password":"password"})
         self.client.credentials(HTTP_AUTHORIZATION=f"Bearer {response.data['token']}")
 
+
+
+class TestListCreateTodos(TodosAPITestCase):
+
     def test_should_not_create_todo_with_no_user_authentication(self):
-        sample_todos = {
-            'title':'test',
-            'description': 'hello world'
-        }
-        response = self.client.post(reverse('todos'),sample_todos)
+        response = self.create_todo()
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
 
 
     def test_should_create_todo(self):
         previous_todo_count = Todo.objects.all().count()
         self.authenticate()
-        sample_todos = {
-            'title':'test',
-            'description': 'hello world'
-        }
-        response = self.client.post(reverse('todos'),sample_todos)
+
+        response = self.create_todo()
+
         self.assertEqual(Todo.objects.all().count(), previous_todo_count+1)
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         self.assertEqual(response.data['title'], 'test')
@@ -39,12 +46,51 @@ class TestListCreateTodos(APITestCase):
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertIsInstance(response.data['results'], list)
 
-        sample_todos = {
-            'title':'test',
-            'description': 'hello world'
-        }
-        self.client.post(reverse('todos'), sample_todos)
+        self.create_todo()
 
         res = self.client.get(reverse('todos'))
         self.assertIsInstance(res.data['count'], int)
         self.assertEqual(res.data['count'], 1)
+
+
+class TestTodoDetailAPIView(TodosAPITestCase):
+    def test_retrives_one_item(self):
+        self.authenticate()
+        response = self.create_todo()
+
+        res = self.client.get(reverse('todo-detail', kwargs={'id':response.data['id']}))
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+
+        todo = Todo.objects.get(id=response.data['id'])
+
+        self.assertEqual(todo.title, res.data['title'])
+
+
+    def test_updates_one_item(self):
+        self.authenticate()
+        response = self.create_todo()
+
+        res = self.client.patch(reverse('todo-detail', kwargs={'id':response.data['id']}), {
+            'title':'new one', 'is_complete': True
+        })
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+
+        updated_todo = Todo.objects.get(id=response.data['id'])
+        self.assertEqual(updated_todo.is_complete, True)
+        self.assertEqual(updated_todo.title, 'new one')
+
+    def test_deletes_one_item(self):
+        self.authenticate()
+        response = self.create_todo()
+        prev_db_count = Todo.objects.all().count()
+
+        self.assertGreater(prev_db_count, 0)
+        self.assertEqual(prev_db_count, 1)
+
+        res = self.client.delete(reverse('todo-detail', kwargs={'id':response.data['id']}))
+
+        self.assertEqual(res.status_code, status.HTTP_204_NO_CONTENT)
+
+        self.assertEqual(Todo.objects.all().count(), 0)
+
+
